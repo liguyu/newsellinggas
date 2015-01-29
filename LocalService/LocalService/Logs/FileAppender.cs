12a76663df0e6a8b0f03cc81a1e4b73c41982f19
shuchun1989 <183334543@qq.com>
2015-01-29 19:37:37 +0800

    卡服务中输出日志到文件中的代码加固，

--- LocalService/LocalService/Logs/FileAppender.cs
+++ LocalService/LocalService/Logs/FileAppender.cs
@@ -6,19 +6,31 @@ using System.Text;
 
 namespace Com.Aote.Logs
 {
-    public class FileAppender :IAppender
+    public class FileAppender : IAppender
     {
 
         public void ShowMessage(string msg)
         {
-            lock(this){
+            //检查是否有当天日期名称文件，没有创建，已创建，追加
+            //System.IO.FileStream fs = null;
+            StreamWriter sw = null;
+            try
+            {
                 //检查是否有当天日期名称文件，没有创建，已创建，追加
                 string fileName = DateTime.Now.ToString("yyyy-MM-dd");
                 string path = "C:/WCSlogs/" + fileName + "WCS.txt";
                 byte[] data = new UTF8Encoding().GetBytes(msg);
-                StreamWriter sw = new StreamWriter(path, true, Encoding.GetEncoding("GB2312"));
+                sw = new StreamWriter(path, true, Encoding.GetEncoding("GB2312"));
                 sw.WriteLine(msg);
-                sw.Close();
+            }
+            catch (Exception e)
+            {
+
+            }
+            finally
+            {
+                if (sw != null)
+                    sw.Close();
             }
         }
     }
